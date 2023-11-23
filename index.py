16fe4c4ffc9704932986340fa1f1a1698a9191f9
Noster <nico.noster@red6-es.de>
2023-11-23 13:57:06 +0100

    example print for comment

--- index.py
+++ index.py
@@ -10,7 +10,14 @@ load_dotenv()
 json_file = os.getenv('JSON_FILE')
 encoding = os.getenv('ENCODING')
 comments = sort_json(load_json(json_file, encoding), json_file)
-# print(comments["1"][1])
+
+print("----- Beispielausgabe für einen Kommentar -----")
+goae_number = "1" #please only give string; can contain letters too.
+comment_number = 0
+# example how to call a specific comment (see end str)
+print(f"GOÄ-Ziffer: {goae_number}\nKommentar-Nr: {comment_number+1}/{len(comments[goae_number])} \
+      \n\nKommentar: {comments[goae_number][comment_number]}\n\n")
+
 
 # connect with open ai api
 prompter = OpenAIPrompter()
