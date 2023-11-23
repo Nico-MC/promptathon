17b5539ed9f713d13d135ba0b22eac90c6ee7a20
Noster <nico.noster@red6-es.de>
2023-11-23 12:28:40 +0100

    add sort json to group comments after goä number

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
-from src.helpers.json_loader import load_json
+from src.helpers.json_loader import load_json, sort_json
 from src.templates.openai_prompter import OpenAIPrompter
+import json
 
 # load environment variables (.env)
 load_dotenv()
@@ -9,9 +10,13 @@ load_dotenv()
 # load json file
 json_file = os.getenv('JSON_FILE')
 encoding = os.getenv('ENCODING')
-load_json(json_file, encoding)
+comments = load_json(json_file, encoding)
+sort_json(comments, json_file)
 
 # connect with open ai api
 prompter = OpenAIPrompter()
-result = prompter.create_completion("Joshua ist ein", 40)
-print(result)
+# result = prompter.create_completion("Joshua ist ein", 40)
+# print(result)
+
+
+
