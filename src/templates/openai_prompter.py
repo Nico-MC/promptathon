0a45dd5e5bbec1842264e2f7358a17ac8db29041
Noster <nico.noster@red6-es.de>
2023-11-23 16:33:04 +0100

    add chat function in prompter

--- src/templates/openai_prompter.py
+++ src/templates/openai_prompter.py
@@ -15,14 +15,26 @@ class OpenAIPrompter:
         except Exception as e:
             print(f"There was a problem connecting to OpenAI API. Please consider checking .env.\nError message: {e}")
 
-    def create_completion(self, prompt: str, max_tokens: int, model="TestDeployment") -> str:
+    def create_completion(self, prompt: str, max_tokens: int, temperature: float, model="TestDeployment") -> str:
         try:
             if self._client is None:
-                print(f"{ConsoleColors.FAIL}_client is none. Please check AzureOpenAI initialisation.{ConsoleColors.ENDC}\n")
+                print(f"{ConsoleColors.FAIL}_client is none. Please check AzureOpenAI initialization.{ConsoleColors.ENDC}\n")
                 return None
             
-            response = self._client.completions.create(model=model, prompt=prompt, max_tokens=max_tokens)
+            response = self._client.completions.create(model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
             return response.choices[0].text
         except Exception as e:
-            print(f"Ein Fehler ist aufgetreten: {e}")
+            print(f"An error occurred on sending create_completion: {e}")
+            return None
+        
+    def create_chat(self, prompt: str, max_tokens: int, temperature: float, model="TestDeployment") -> str:
+        try:
+            if self._client is None:
+                print(f"{ConsoleColors.FAIL}_client is none. Please check AzureOpenAI initialization.{ConsoleColors.ENDC}\n")
+                return None
+            
+            response = self._client.completions.create(model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
+            return response.choices[0].text
+        except Exception as e:
+            print(f"An error occurred on sending create_chat: {e}")
             return None
