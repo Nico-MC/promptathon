c59a96b3ed562f893ab2d46a61c14d6c80c441fa
Noster <nico.noster@red6-es.de>
2023-11-28 16:30:01 +0100

    adjust json loader for new script

--- src/templates/openai_prompter.py
+++ src/templates/openai_prompter.py
@@ -33,8 +33,25 @@ class OpenAIPrompter:
                 print(f"{ConsoleColors.FAIL}_client is none. Please check AzureOpenAI initialization.{ConsoleColors.ENDC}\n")
                 return None
             
-            response = self._client.completions.create(model=model, prompt=prompt, max_tokens=max_tokens, temperature=temperature)
-            return response.choices[0].text
+            print(f"{ConsoleColors.OKCYAN}Send following prompt:\n{prompt}{ConsoleColors.ENDC}")
+            response = self._client.chat.completions.create(
+                model=model,
+                messages=[
+                    # system: Dies definiert eine Systemnachricht, die Anweisungen oder Informationen für das Modell enthält, wie es antworten soll.
+                    {"role": "system", "content": "Du bist ein Assistent für einen Sachbearbeiter und solltst den gegebenen Text in Stichworte kategorisieren."},
+                    # user: Nachricht des Benutzers
+                    {"role": "user", "content": prompt},
+                    # assistant Dies sind Antworten, die das Modell in früheren Dialogen generiert hat. Sie geben Kontext und zeigen,
+                    # wie das Modell auf vorherige Benutzeranfragen reagiert hat.
+                    {"role": "assistant", "content": """
+                     Hier eine Beispielauflistung, von einem vorherigen Ergebnis:
+                     1. Abrechnung
+                     2. Stationäre Behandlung
+                     3. Leistungsnummern"""},
+                    # {"role": "user", "content": "Where was it played?"}
+                ]
+            )
+            return response.choices[0].message.content
         except Exception as e:
             print(f"An error occurred on sending create_chat: {e}")
             return None
