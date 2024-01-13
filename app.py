22304bc81150c54594a1e7d5436c1825ba0549e8
Noster <nico.noster@red6-es.de>
2024-01-14 00:00:20 +0100

    frontend progress

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request
-import index
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from src.init import *
 
-app = Flask(__name__, template_folder='src/templates')
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/submit-prompts', methods=['POST'])
-def submit_prompts():
-    prompt_count = int(request.form.get('promptCount', 0))
-    prompts = [request.form.get(f'prompt{i}') for i in range(prompt_count)]
-    return "Prompts erhalten: " + str(prompts)
+@app.route('/getCategoriesForGoaeNumber', methods=['POST'])
+def get_categories_for_goae_number():
+    data = request.get_json()
+    print(data)
+    numbers = data['numbers']
+    if len(numbers) < 1:
+        return
+    prompt_count = data['promptCount']
+    prompts = data['prompts']
+    if prompt_count == "1":
+        write_comments_after_prefix(numbers)
+        json_data = read_json("group_comments_after_prefix.json")
+        json_data = get_categories_for_prefix(json_data, numbers)
+        write_categories_in_json("categories_for_prefix.json", json_data)
+        return jsonify(json_data)
+    exit()
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
